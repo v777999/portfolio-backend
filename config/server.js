52f491b5f7fd51dd6774e642638e989f07420d12
v777999 <55867385+v777999@users.noreply.github.com>
2021-07-01 08:29:51 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa7778e9c76bff758649f5804a3802d7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '686ac35d-8bb4-4fb0-9bc2-e34df3ba8b21'
+};
